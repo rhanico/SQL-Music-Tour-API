18bcf38f18b2374e85b5c82226738c6423761c75
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-10-04 12:37:45 -0700

    initial commit

--- server.js
+++ server.js
@@ -2,10 +2,27 @@
 const express = require('express')
 const app = express()
 
+const { Sequelize } = require('sequelize')
+
+
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate();
+    console.log( "CONNECTED TO POSTGRES" )
+} catch(err) {
+    console.log( `Unable to Connect to DB: ${err}` )
+}
+
+
+
+
 
 // ROOT
 app.get('/', (req, res) => {
